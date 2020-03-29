1e817fece69cadb32ed59f423ba875a5aabcb079
claus.e.andersen <clan@dtu.dk>
2020-03-29 18:03:32 +0200

    Again

--- R/convert-hex-function.R
+++ R/convert-hex-function.R
@@ -1,10 +1,54 @@
-#' @title Convert hex strings to integers (needed in CRC32 computations)
-#' Does not break the 2^31 limit.
+#' @title convert.hex Robust prepanel function for lattice plots (xlim).
+#' @description
+#' This prepanel function can provide useful scales even if there is
+#' only one data point or if all values are identical (no variability).
+#' A limit parameter equal to 0.01 means that the scale will at least
+#' go from 1-limit = 0.99 to 1+limit = 1.01 times the reference value.
+#' The parameter limit.robust is for the situation when there is only one data point
+#' that is not NA. The parameter limit.mean uses the mean as reference, and
+#' limit.median uses the median value as reference.
+#' Note that the lattice plots generally fail (without a prepanel function) if there is only
+#' one single point and if that value is very large (for example, y=6e66).
+#' The following prepanel functions can be used to handle such situations.
+#' The prepanel function can also be used to ascertain that a useful
+#' minimum scale of say +/- 0.5\% is always shown. Note that the actual
+#' scale may be larger (i.e. the selected scale limits will always include the
+#' data).
+#' See also \link{prepanel.robust.y} and \link{prepanel.robust.xy}
+#' Example:
+#'
+#' N <- 40 # two of which are NA-values
+#'
+#' df <- data.frame(model= sample(c("M1"),size=N,replace=TRUE), cap= sample(c("A","B","C"),size=N,replace=TRUE),
+#' pA = c(NA,NA,rnorm(N-2,mean=9e99,sd=0.001)))
+#'
+#' dotplot(cap ~ pA | paste(model),
+#'
+#' limit.mean = 0.05, # Make sure we have an x-scale of at least +/- 5\% of the mean
+#'
+#' prepanel=prepanel.robust.x,
+#'
+#' groups=cap,
+#'
+#' auto.key=list(TRUE),
+#'
+#' data=df,
+#'
+#' panel=function(x,y,...){
+#'
+#' panel.dotplot(x,y,...)
+#'
+#' })
+#' @usage none
 #' @name convert.hex
-#' @param Hex string to be converted.
-#'
-#' @return CRC32 value
-#'
+#' @author Claus E. Andersen
+#' @return a list with xlim-values for a trellis plot.
+#' @param x gives the x-values.
+#' @param y gives the y-values.
+#' @param ... for additional parameters.
+#' @param limit.robust limit to use is there is only one data point.
+#' @param limit.mean is the mean-based minimum limit.
+#' @param limit.median is the median-based minimum limit.
 #' @export convert.hex
 convert.hex <- function(x) {
   # This function converts hex strings to integers
